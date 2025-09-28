8d7d785458ec8ca6fe787f0536f5569683e6b393
Kyrylo Shestakov <freyr.sh@gmail.com>
2025-09-28 23:44:38 +0300

    add support for debugging

--- main.go
+++ main.go
@@ -1,14 +1,25 @@
 package main
 
 import (
+	"flag"
+
 	"github.com/dmacvicar/terraform-provider-libvirt/libvirt"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
+)
+
+var (
+	flagDebug = flag.Bool("debug", false, "enable debugger support")
+	flagProiverAddr = flag.String("addr", "registry.terraform.io/cyril-s/libvirt", "full proivder address as written in the terraform configuration")
 )
 
 func main() {
 	defer libvirt.CleanupLibvirtConnections()
 
+	flag.Parse()
+
 	plugin.Serve(&plugin.ServeOpts{
+		Debug: *flagDebug,
+		ProviderAddr: *flagProiverAddr,
 		ProviderFunc: libvirt.Provider,
 	})
 }
